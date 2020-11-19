227927d7b25a2e09283217cc08785584271bf7bc
unknown <juliomruiz17@gmail.com>
2020-11-19 16:06:03 -0300

    construçao

---
+++ CodersAcademy/CodersAcademy.API/Controllers/AlbumController.cs
@@ -0,0 +1,29 @@
+﻿using CodersAcademy.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodersAcademy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        private AlbumRepository Repository { get; set; }
+
+        public AlbumController(AlbumRepository repository)
+        {
+            this.Repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAlbuns()
+        {
+            return Ok(await this.Repository.GetAllAsync());
+        }
+
+    }
+}
